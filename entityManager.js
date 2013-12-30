dcaa00aee27c2a714c9867a4ca28d13d85f9acf4
Ivan <ivan@ivan-SATELLITE-C660.(none)>
2013-12-30 15:45:35 +0000

    entityFix

--- entityManager.js
+++ entityManager.js
@@ -4,7 +4,8 @@ var entityManager =
 	viewY : 100,
 	updateRadius : 500,
 	player : undefined, 
-	objects : []
+	enemies : [],
+	obstacles: []
 }
 
 entityManager.setPlayer = function(descr)
@@ -12,9 +13,9 @@ entityManager.setPlayer = function(descr)
 	this.player = new Player(descr);
 }
 
-entityManager.add = function(obj)
+entityManager.addObstacle = function(obj)
 {
-	this.objects[this.objects.length]=obj;
+	this.obstacles[this.obstacles.length]=obj;
 }
 
 entityManager.update = function(du) 
@@ -25,14 +26,12 @@ entityManager.update = function(du)
 		this.viewY = this.player.getY();
 	}
 
-	this.obstacles=[];
-	for(var i=0; i<this.objects.length; i++)
+	for(var i=0; i<this.obstacles.length; i++)
 	{
-		if( distSq( this.objects[i].cx,  this.objects[i].cy,  this.viewX,  this.viewY) < Math.pow(this.updateRadius,2) )
+		if( distSq( this.obstacles[i].cx,  this.obstacles[i].cy,  this.viewX,  this.viewY) < Math.pow(this.updateRadius,2) )
 		{
-			var status = this.objects[i].update(du);
-			if(status==="die") {this.objects.splice(i,1);}
-			else if(status==="obs"){this.obstacles.push(this.objects[i]);}
+			var status = this.obstacles[i].update(du);
+			if(status==="die") {this.obstacles.splice(i,1);}
 		}
 	}
 
@@ -41,11 +40,11 @@ entityManager.update = function(du)
 
 entityManager.render = function(ctx) 
 {
-	for(var i=0; i<this.objects.length; i++)
+	for(var i=0; i<this.obstacles.length; i++)
 	{
-		if( distSq( this.objects[i].cx,  this.objects[i].cy,  this.viewX,  this.viewY) < Math.pow(this.updateRadius,2) )
+		if( distSq( this.obstacles[i].cx,  this.obstacles[i].cy,  this.viewX,  this.viewY) < Math.pow(this.updateRadius,2) )
 		{
-			this.objects[i].render(ctx,this.viewX-g_canvas.width/2,this.viewY-g_canvas.height/2);
+			this.obstacles[i].render(ctx,this.viewX-g_canvas.width/2,this.viewY-g_canvas.height/2);
 		}
 	}
 	this.player.render(ctx,this.viewX-g_canvas.width/2,this.viewY-g_canvas.height/2);
